dbb16e2738fb8b341db78601e8e9f125f603c499
fardin.masoudi <70ardin@gmail>
2022-03-14 16:46:21 +0330

    fix: move login controller to Auth folder

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\ApiController;
 use App\Http\Requests\LoginRequest;
 use App\Interfaces\UserRepositoryInterface;
 use Illuminate\Support\Facades\Auth;
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
