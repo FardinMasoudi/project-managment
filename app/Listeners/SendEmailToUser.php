855041c20d8358304da9fa7f35a162857bca218a
fardin.masoudi <70ardin@gmail>
2022-03-15 14:30:22 +0330

    make event and listener for invitation

---
+++ app/Listeners/SendEmailToUser.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\Invite;
+use App\Mail\WellcomeInvitationMail;
+use Illuminate\Support\Facades\Mail;
+
+class SendEmailToUser
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param \App\Events\Invite $event
+     * @return void
+     */
+    public function handle(Invite $event)
+    {
+        Mail::to($event->user->email)->send(
+            new WellcomeInvitationMail()
+        );
+    }
+}
