66f66a1e89b6305c73b67d72a9442c35393c3bc9
fardin.masoudi <70ardin@gmail>
2022-03-17 19:13:38 +0330

    feat: set schedule expired tasks to kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,15 +7,19 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        Commands\CheckTaskIsExpired::class,
+    ];
+
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('task:expired')->daily();
     }
 
     /**
@@ -25,7 +29,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
