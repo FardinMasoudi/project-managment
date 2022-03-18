718a48ffb786139b85c8e7f5b3477b3dfaffea5a
fardin.masoudi <70ardin@gmail>
2022-03-18 15:34:03 +0330

    feat: mock expire task

--- tests/Feature/Command/TaskExpireTest.php
+++ tests/Feature/Command/TaskExpireTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Feature\Command;
 
+use App\Mail\ExpiredTaskEmail;
+use App\Mail\WelcomeInvitationMail;
 use App\Models\Task;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
 
 class TaskExpireTest extends TestCase
@@ -22,6 +25,8 @@ class TaskExpireTest extends TestCase
 
     public function test_check_doing_tasks_is_expired()
     {
+        Mail::fake();
+
         $this->create(Task::class, [
             'deadline_time' => Carbon::now()->subDay()
         ]);
@@ -31,5 +36,7 @@ class TaskExpireTest extends TestCase
 
         $this->artisan('task:expired');
 
+
+        Mail::assertSent(ExpiredTaskEmail::class);
     }
 }
